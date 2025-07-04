a24143e91ec241e86e55e9c2ff29a92b12d79ce0
Tri Lai <laitrantri123@gmail.com>
2025-07-04 19:39:13 +0700

    Add Single Number

--- Single Number/mysolution.cpp
+++ Single Number/mysolution.cpp
@@ -1,8 +1,15 @@
 /*                          
                                     ************This is the most optimized algorithm to solve this problem************
-    Using bitwise operator "xor" - "^" ( apply on each bit )
-    a^0 = a ( Ex: 1010 xor 0000 = 1010 )
-    a^a = 0 ( EX: 1010 xor 1010 = 0000 )
+    Using bitwise operator "XOR" - "^" ( apply on each bit )
+    Formula:
+    a^0 = a ( Ex: 1010 XOR 0000 = 1010 )
+    a^a = 0 ( EX: 1010 XOR 1010 = 0000 )
+    Every pair of element will result 0 when using XOR, finally res = the single one
+
+    Ex:
+    2 4 1 4 2
+    Make pair: 2 XOR 2 = 0 and 4 XOR 4 = 0  
+    --> we will find the single one : res = 0 XOR 1 = 1
 */
 
 int singleNumber(vector<int>& nums) { // main function
